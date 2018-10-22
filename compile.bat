9582e78cbbb2878bef7b6316a96ab4bddb9a8427
unknown <vlade.maksimovski@gmail.com>
2018-10-22 16:37:49 -0400

    Added directories again

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ -O4 src/*.cpp -o ChessGame -std=c++14 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system 
+g++ -I/include -O4 src/*.cpp -o ChessGame -std=c++14 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
